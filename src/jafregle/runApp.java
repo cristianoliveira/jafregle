af3a4a3ec95989f71406a68ad3a9e1f84337fb21
Cristian Oliveira <contato@cristianoliveira.com.br>
2015-01-13 17:30:33 -0200

    Gradle adjusts

--- src/jafregle/runApp.java
+++ src/jafregle/runApp.java
@@ -4,13 +4,13 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
-public class runApp {
+public class RunApp {
 	public static void main(String[] args) {
         try {
             //RUN! Camingoal! Camigoal!
             System.out.println(Jafregle.translate("Hello", Jafregle.Language.ENGLISH, Jafregle.Language.PORTUGUESE));
         } catch (Exception ex) {
-            Logger.getLogger(runApp.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(RunApp.class.getName()).log(Level.SEVERE, null, ex);
         }
 	}
 
