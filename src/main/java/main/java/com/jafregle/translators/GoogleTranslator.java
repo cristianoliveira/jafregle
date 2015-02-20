f0bd298dfb067c00b6d653b66b622de6beedc3ee
Cristian Oliveira da Rosa <contato@cristianoliveira.com.br>
2015-02-20 01:11:24 -0200

    Fix identation issue

--- src/main/java/main/java/com/jafregle/translators/GoogleTranslator.java
+++ src/main/java/main/java/com/jafregle/translators/GoogleTranslator.java
@@ -8,27 +8,25 @@ public class GoogleTranslator implements ITranslator{
 
 	final String GOOGLE_API_URL = "https://www.googleapis.com/language/translate/v2?";
 	final String GOOGLE_API_PARAMS = "key=%s&q=%s&source=%s&target=%s";
-	
+
 	String googleKey;
-	
-	public void setGoogleKey(String googleKey)
+
+	public GoogleTranslator(String googleKey)
 	{
 		this.googleKey = googleKey;
 	}
-	
+
 	@Override
 	public String requestTranslation(String textToTranslate, String from, String to) throws IOException {
-		
-		if(googleKey.isEmpty())
-			new IllegalArgumentException("Google Key must to be informed.");
-		
-		String encodedText = java.net.URLEncoder.encode(textToTranslate, "UTF-8");
-	    
-		String params      = String.format(GOOGLE_API_PARAMS, googleKey, encodedText, from, to);
-	    
-	    String result      = new WebHelper().access(GOOGLE_API_URL, params);
-	    
-       return result;
+
+        if(googleKey.isEmpty())
+            new IllegalArgumentException("Google Key must be informed.");
+
+        String encodedText = java.net.URLEncoder.encode(textToTranslate, "UTF-8");
+        String params      = String.format(GOOGLE_API_PARAMS, googleKey, encodedText, from, to);
+
+        String result      = new WebHelper().access(GOOGLE_API_URL, params);
+        return result;
 	}
 
 }
